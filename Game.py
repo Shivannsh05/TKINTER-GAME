c21c482aafbe344597664a512a0cd4fafbdeb46d
Samarth <samarthk292@gmail.com>
2024-12-22 13:45:12 +0530

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,46 +1,49 @@
 import tkinter as tk
-import pygame
+from tkinter import messagebox
+import pygame 
 
 story_data = {
-    "display":{
-        "text" : "HEY!, I AM ALICE AND I WILL BE WATCHING YOU THROUGH OUT THE GAME.\nYOUR CHOICES IN GAME WILL HELP ME TO KNOW ABOUT YOUR PERSONALITY",
-        "CHOICE" : {
-            "ADVANCE" : "start"
+    "display": {
+        "text": "HEY!, I AM ALICE AND I WILL BE WATCHING YOU THROUGH OUT THE GAME.\nYOUR CHOICES IN GAME WILL HELP ME TO KNOW ABOUT YOUR PERSONALITY",
+        "CHOICE": {
+            "ADVANCE": "start"
         }
     },
-    "start":{
-        "text" : "YOUR ARE ST3012, NICKNAMED AS CONNER, ONE OF THE MOST ADVANCE DETROIT MADE CYBERLIFE. YOUR ARE PROGRAMMED TO SOLVE COMPLEX CASE.\nYou are assigned a case of detroit who assaulted the the owner and ran away with his daughter.\n   REMEMBER:-THE CHOICES YOU MAKE WILL DECIDE THE FUTURE",
-        "CHOICE" : {"PLAY" : "house"}
+    "start": {
+        "text": "YOUR ARE ST3012, NICKNAMED AS CONNER, ONE OF THE MOST ADVANCE DETROIT MADE CYBERLIFE. YOUR ARE PROGRAMMED TO SOLVE COMPLEX CASE.\nYou are assigned a case of detroit who assaulted the the owner and ran away with his daughter.\n   REMEMBER:-THE CHOICES YOU MAKE WILL DECIDE THE FUTURE",
+        "CHOICE": {"PLAY": "house"}
     },
-    "house" :{
-        "text" : "YOU ARE AT THE CRIME SCENE, WHAT DO WANT TO DO?",
-        "CHOICE" : {
-            "INVESTIGATE THE CRIME SCENE" : "investigate",
-            "TALK TO HANK" : "hank"
+    "house": {
+        "text": "YOU ARE AT THE CRIME SCENE, WHAT DO WANT TO DO?",
+        "CHOICE": {
+            "INVESTIGATE THE CRIME SCENE": "investigate",
+            "TALK TO HANK": "hank"
         }
     },
-    "investigate" : {
-        "text" : "INVESTIGATE THE CRIME SCENE AND DEAD BODY AND FIND THE GIRL",
-        "CHOICE" : {
-            "SCAN THE PHOTO" : "scan",
-            "CHECK THE DEADY body" : "dead",
-            "SCAN THE WALL" : "wall"
+    "investigate": {
+        "text": "INVESTIGATE THE CRIME SCENE AND DEAD BODY AND FIND THE GIRL",
+        "CHOICE": {
+            "SCAN THE PHOTO": "scan",
+            "CHECK THE DEADY body": "dead",
+            "SCAN THE WALL": "wall"
         }
     },
-    "hank" : {
-        "text" : "HANK TELL ABOUT THE NUMBER OF INCREASE IN CASES SIMILAR TO THIS",
-        "CHOICE" : {
-            "ASK ABOUT DETROIT" : "detroit",
-            "COMMON THING IN ALL CASES" : "common"
+    "hank": {
+        "text": "HANK TELL ABOUT THE NUMBER OF INCREASE IN CASES SIMILAR TO THIS",
+        "CHOICE": {
+            "ASK ABOUT DETROIT": "detroit",
+            "COMMON THING IN ALL CASES": "common"
         }
     }
 }
+
 def update_story(scene):
     story = story_data[scene]
     story_text.set(story["text"])
 
+    # If the scene is 'start', start the background audio
     if scene == "house":
-        play_audio()
+        play_background_audio()
 
     for widget in choice_button_frame.winfo_children():
         widget.destroy()
@@ -57,15 +60,13 @@ def update_story(scene):
             font=("Arial", 12, "bold"), 
             relief="raised"   
         )
-        choice_button.pack(pady=5) 
+        choice_button.pack(pady=5)  # Add some spacing between buttons
 
-#background audio
-def play_audio():
+def play_background_audio():
     pygame.mixer.init()
-    pygame.mixer.music.load("risk.mp3") 
+    pygame.mixer.music.load("risk.mp3")  
     pygame.mixer.music.set_volume(0.5)  
-    pygame.mixer.music.play(-1, 0.0) 
-
+    pygame.mixer.music.play(-1, 0.0)  
 
 conner = tk.Tk()
 conner.title("DETROIT: JUST A MACHINE?")
